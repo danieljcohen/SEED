27f70eb232f08048877d9e09e0240a94cbc8e7cb
Daniel Cohen <djc86@duke.edu>
2023-09-08 23:11:16 -0400

    Removed redundant homepage file, started Card component and homepage implementation

--- Front End/src/components/pages/Homepage.js
+++ Front End/src/components/pages/Homepage.js
@@ -11,23 +11,7 @@ function Homepage() {
     return (
         <>
             <div className="Homepage-container">
-                <video src = "/hd0992.mov" autoPlay loop muted />
-                <div className="info-but"> 
-                    <div className="info">
-                        WE FOCUS ON PROVIDING RISK-AVERSE INVESTMENT SOLUTIONS BY UTILIZING
-                        DISRUPTIVE TECHNOLOGY TO MAXIMIZE VALUE AND DRIVE RETURNS PRIMARILY
-                        IN DIGITAL INFRASTRUCTURE.
-                    </div>
-                    <div className="home-button">
-                        <Link to="/about">
-                            <button class="button">
-                                <div className="button-text">
-                                LEARN MORE ABOUT US
-                                </div>
-                            </button> 
-                        </Link>
-                    </div>
-                </div>
+                
             </div>
         </>
     );
