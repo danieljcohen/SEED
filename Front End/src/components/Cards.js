e70e11aed3d3aae2afdb85da41cca648171418ae
Daniel Cohen <djc86@duke.edu>
2023-09-09 17:38:39 -0400

    navbar not work

--- Front End/src/components/Cards.js
+++ Front End/src/components/Cards.js
@@ -10,9 +10,9 @@ function Cards(){
     equity: percent for price
     */
     return (
-        <div className="cards">
+        <div className="main-container">
             <h1>Businesses</h1>
-            <div>
+            <div classname="cards">
                 <CardItem companyName="Google" logo='./google.png' bio = "bla bla bla google bio goes here and this has to be longer so im typing" equity = "10% for 3 bucks" />
             </div>
         </div>
