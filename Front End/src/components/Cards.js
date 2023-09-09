1831be63d1089c664e07f63f0893ebb00e5e1cd7
Daniel Cohen <djc86@duke.edu>
2023-09-08 23:43:12 -0400

    More updates to card components, added some vars

--- Front End/src/components/Cards.js
+++ Front End/src/components/Cards.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import CardItem from './CardItem'
+import './Cards.css'
 
 function Cards(){
     return (
         <div className="cards">
-            <h1>Company Name</h1>
+            <h1>Businesses</h1>
             <div className="cards_container">
                 <div className="cards_wrapper">
                     <ul className="cards_items">
