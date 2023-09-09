eaa25ae3f2cfb14eba61ab9ee47b433dd6539cdb
Daniel Cohen <djc86@duke.edu>
2023-09-09 15:09:06 -0400

    Fixed cardItem formatting

--- Front End/src/components/CardItem.js
+++ Front End/src/components/CardItem.js
@@ -10,14 +10,10 @@ export default function CardItem(props){
     bio: company bio
     equity: percent for price
     */
-
-export default function Card(props){
-    return(
-        <div className="card_item_border">
-            <h1 className="card_item_company_name">{props.companyName}</h1>
-            <img src={require('./google.png')} className="card_item_logo"></img>
-            <p className="card_item_bio">{props.bio}</p>
-            <h5 className="">Asking for {props.equity}</h5>
-        </div>
-    )
+    <div className="card_item_border">
+        <h1 className="card_item_company_name">{props.companyName}</h1>
+        <img src={require('./google.png')} className="card_item_logo"></img>
+        <p className="card_item_bio">{props.bio}</p>
+        <h5 className="">Asking for {props.equity}</h5>
+    </div>
 }
